a02e544e3c2d8517996a8357f7e205873bf9d6c4
Muhammed GÜNER <guner.muhammed@outlook.com>
2024-05-25 13:24:17 +0300

    Add project files.

---
+++ Entities/Concrete/App.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Core.Entities;
+using System;
+
+namespace Entities.Concrete
+{
+    public class App : IEntity
+    {
+        public int Id { get; set; }
+        public string AppName { get; set; }
+        public string AppStoreURL { get; set; }
+        public string PlayStoreURL { get; set; }
+    }
+}
